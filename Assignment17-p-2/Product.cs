b9175452fab4c7a0481e69c6a276921e4128f8dc
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-11-21 16:09:50 +0530

    Assignment17-p-2

---
+++ Assignment17-p-2/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment17_p_2
+{
+    public class Product
+    {
+       public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine($"Product ID: {id}");
+            Console.WriteLine($"Product Name: {name}");
+            Console.WriteLine($"Product Price: {price}");
+        }
+
+        public virtual void SetInformation(int id, string name, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
+
+
---
+++ Assignment17-p-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17_p_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HerbalProduct herbalProduct = new HerbalProduct();
+            herbalProduct.SetInformation(1, "Herbal Shampoo", 650.50, "Aloe Vera",
+            DateTime.Parse("2023-10-13"), DateTime.Parse("2025-01-04"));
+            Console.WriteLine("Herbal Product Details:");
+            herbalProduct.ShowDetails();
+            Console.ReadKey();
+        }
+    }
+}
+
